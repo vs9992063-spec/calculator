97bf7ca973c5c64529bd9ce9831d4b2dbc81576d
vs9992063-spec <vs9992063@gmail.com>
2026-09-17 15:31:35 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+addition = a + b
+subtraction = a - b
+
+print("Addition =", addition)
+print("Subtraction =", subtraction)
