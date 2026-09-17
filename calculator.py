13c34ad58457f2c113e116363bc5e079e6a62bc6
vaishnavishetty <vs9992063@gmail.com>
2026-09-17 16:21:27 +0530

    Add multiplication and division functionality

--- calculator.py
+++ calculator.py
@@ -4,7 +4,9 @@ b = int(input("Enter second number: "))
 addition = a + b
 subtraction = a - b
 multiplication = a * b
+division = a/b
 
 print("Addition =", addition)
 print("Subtraction =", subtraction)
 print("Multiplication =",multiplication)
+print("Division =",division)
